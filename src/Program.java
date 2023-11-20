fef72acab6742629a3fdb8d26d69cc22fb2d2b53
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2023-11-19 21:49:06 -0300

    Segundo commit
    Fazendo comentários

--- src/Program.java
+++ src/Program.java
@@ -8,16 +8,19 @@ public class Program {
         System.out.println("Quantos valores cada vetor vai ter ");
         int n = sc.nextInt();
         int[] vetor = new int[n];
+        //Vetor A
         System.out.println("Digite os valores do vetor A ");
         int[] vetorA = new int[n];
         for (int i = 0; i < n; i++) {
             vetorA[i] = sc.nextInt();
         }
+        //Vetor B
         System.out.println("Digite os valores do vetor B ");
         int[] vetorB = new int[n];
         for (int i = 0; i < n; i++) {
             vetorB[i] = sc.nextInt();
         }
+        //Vetor C
         System.out.println("Vetor resultante, que é a soma dos elementos de A e B ");
         int[] vetorResultante = new int[n];
         for (int i = 0; i < n; i++) {
